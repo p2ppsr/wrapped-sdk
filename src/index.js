3692642ac1b57d994acc6f692a8417d404d1cecc
Brayden Langley <brayden167@gmail.com>
2024-01-10 11:55:00 -0800

    Support running in a node environment

--- src/index.js
+++ src/index.js
@@ -1,69 +1,74 @@
 const SDK = require('@babbage/sdk')
 
-const style = document.createElement('style')
+// Check if we're running in a browser or node environment
+const documentAvailable = (typeof window === 'object' && typeof document === 'object')
 
-style.innerHTML = `
-.wrapped-babbage-sdk-modal {
-  top: 0%;
-  left: 0%;
-  width: 100vw;
-  height: 100vh;
-  position: absolute;
-  background: rgba(0, 0, 0, 0.5);
-  transition: all 0.3s;
-  opacity: 0;
-}
-.wrapped-babbage-sdk-child {
-  top: 50%;
-  left: 50%;
-  max-width: 80vw;
-  max-height: 80vh;
-  overflow: scroll;
-  background: #333333;
-  border-radius: 0.5em;
-  color: white;
-  position: absolute;
-  transform: translate(-50%, -50%);
-  font-family: helvetica;
-}
-.wrapped-babbage-sdk-close {
-  position: absolute;
-  top: 0;
-  right: 0;
-  color: white;
-  font-size: 2em;
-  background: none;
-	color: inherit;
-	border: none;
-	padding: 0.5em 0.7em;
-	font: inherit;
-	cursor: pointer;
-	outline: inherit;
-}
-.wrapped-babbage-sdk-content {
-  padding: 0.5em 1em;
-}
-.wrapped-babbage-sdk-link {
-  border: 1px solid white;
-  border-radius: 0.2em;
-  transition: all 0.2s;
-  color: white;
-  cursor: pointer;
-  text-decoration: none;
-  padding: 0.5em;
-  font-weight: bold;
-  text-transform: uppercase;
-  display: block;
-  width: fit-content;
-  margin: 0.5em 0px;
-}
-.wrapped-babbage-sdk-link:hover {
-  color: #333333;
-  background-color: white;
-}
-`
+if (documentAvailable) {
+  const style = document.createElement('style')
 
-document.body.appendChild(style)
+  style.innerHTML = `
+  .wrapped-babbage-sdk-modal {
+    top: 0%;
+    left: 0%;
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    background: rgba(0, 0, 0, 0.5);
+    transition: all 0.3s;
+    opacity: 0;
+  }
+  .wrapped-babbage-sdk-child {
+    top: 50%;
+    left: 50%;
+    max-width: 80vw;
+    max-height: 80vh;
+    overflow: scroll;
+    background: #333333;
+    border-radius: 0.5em;
+    color: white;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    font-family: helvetica;
+  }
+  .wrapped-babbage-sdk-close {
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: white;
+    font-size: 2em;
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0.5em 0.7em;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+  }
+  .wrapped-babbage-sdk-content {
+    padding: 0.5em 1em;
+  }
+  .wrapped-babbage-sdk-link {
+    border: 1px solid white;
+    border-radius: 0.2em;
+    transition: all 0.2s;
+    color: white;
+    cursor: pointer;
+    text-decoration: none;
+    padding: 0.5em;
+    font-weight: bold;
+    text-transform: uppercase;
+    display: block;
+    width: fit-content;
+    margin: 0.5em 0px;
+  }
+  .wrapped-babbage-sdk-link:hover {
+    color: #333333;
+    background-color: white;
+  }
+  `
+
+  document.body.appendChild(style)
+}
 
 const hideModal = () => {
   const modal = document.querySelector('.wrapped-babbage-sdk-modal')
@@ -76,6 +81,11 @@ const hideModal = () => {
 }
 
 const showModal = () => {
+  if (!documentAvailable) {
+    const e = new Error('The operation you are trying to complete can only work if you have a MetaNet identity set up. You can either abort the operation, or set up a MetaNet identity before trying again.')
+    e.code = 'ERR_NO_METANET_IDENTITY'
+    throw e
+  }
   const existingModal = document.querySelector('.wrapped-babbage-sdk-modal')
   if (existingModal) {
     return
